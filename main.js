0613840396ce3373c35868dbed08d4148c2ffe35
Kevin Matthews <kevmatthews3@gmail.com>
2020-05-15 23:30:20 -0400

    enemies

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let score;
 let highscore;
 let player;
 let gravity;
-let obstacles;
+let obstacles = [];
 let gameSpeed;
 let keys = {};
 
@@ -39,6 +39,12 @@ class Player {
       this.Jump();
     } else {
       this.jumpTimer = 0;
+    }
+
+    if (keys["ShiftLeft"] || keys["KeyS"]) {
+      this.height = this.originalHeight / 2;
+    } else {
+      this.h = this.originalHeight;
     }
 
     this.y += this.dy;
@@ -74,6 +80,53 @@ class Player {
   }
 }
 
+class Obstacle {
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+
+    this.dx = -gameSpeed;
+  }
+
+  Update() {
+    this.x += this.dx;
+    this.Draw();
+    this.dx = -gameSpeed;
+  }
+
+  Draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.c;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+    ctx.closePath();
+  }
+}
+
+// Game Functions
+function SpawnObstacle() {
+  let size = RandomIntRange(20, 70);
+  let type = RandomIntRange(0, 1);
+  let obstacle = new Obstacle(
+    canvas.width + size,
+    canvas.height - size,
+    size,
+    size,
+    "#2484E4"
+  );
+
+  if (type == 1) {
+    obstacle.y -= player.originalHeight - 10;
+  }
+  obstacles.push(obstacle);
+}
+
+function RandomIntRange(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
 function Start() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -91,9 +144,28 @@ function Start() {
   requestAnimationFrame(Update);
 }
 
+let initialSpawnTimer = 200;
+let spawnTimer = initialSpawnTimer;
 function Update() {
   requestAnimationFrame(Update);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  spawnTimer--;
+  if (spawnTimer <= 0) {
+    SpawnObstacle();
+    spawnTimer = initialSpawnTimer - gameSpeed * 8;
+
+    if (spawnTimer < 60) {
+      spawnTimer = 60;
+    }
+  }
+
+  // Spawn Enemies
+  for (let i = 0; i < obstacles.length; i++) {
+    let o = obstacles[i];
+
+    o.Update();
+  }
 
   player.Animate();
 }
